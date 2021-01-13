7085e4e5c3e9c3afbea7c304d3eff07018ee7884
nojick <nojick@gmail.com>
2021-01-13 13:19:11 +0300

    msm8916:ido: fix  livedisplay and lag mm-pp-daemon
    mm-pp-daemon loads the CPU to 100% so just disable it. We can still use the live display.
    
    fixed based on comment acroreiser/android_device_lenovo_a6000
    
    https://github.com/acroreiser/android_device_lenovo_a6000/commit/72cfd98261fadbfb911fc8f4adb98144a8743728#diff-247ee86229a709a6e2eedfc1d3c4a557825aee073e20b0112ab76f4ca8e4bc4e

--- msm8916.mk
+++ msm8916.mk
@@ -262,6 +262,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# LiveDisplay
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.0-service-legacymm
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
