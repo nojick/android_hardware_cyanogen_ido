1bad79ddff11485fd4299c27d18322e31ab8ab2c
nojick <nojick@gmail.com>
2021-01-13 13:44:39 +0300

    msm8916:ido: soundtrigger
    I tried different versions of the service, it still doesn't work.

--- msm8916.mk
+++ msm8916.mk
@@ -62,8 +62,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0 \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    android.hardware.soundtrigger@2.1-service \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
