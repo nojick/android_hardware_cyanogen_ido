4539be5484d0bb19f7f1472d4001e86c1ce6d8f1
nojick <nojick@gmail.com>
2021-01-24 14:19:41 +0300

    remove shim gps crackling

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,11 +134,11 @@ BOARD_SEPOLICY_DIRS += \
     $(PLATFORM_PATH)/sepolicy
 
 # Shims
-TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib64/libflp.so|libshims_flp.so \
-    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
-    /system/vendor/lib/libflp.so|libshims_flp.so \
-    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
+#TARGET_LD_SHIM_LIBS := \
+#    /system/vendor/lib64/libflp.so|libshims_flp.so \
+#    /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
+#    /system/vendor/lib/libflp.so|libshims_flp.so \
+#    /system/vendor/lib/libizat_core.so|libshims_get_process_name.so
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
