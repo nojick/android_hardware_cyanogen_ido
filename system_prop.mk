0a747ff5cbf7cbb17ef8c8d164725a1eb50e8304
nojick <nojick@gmail.com>
2021-02-03 16:24:42 +0300

    BOARD_CHARGER_ENABLE_SUSPEND becomes ro.charger.enable_suspendmsm8916: ido: video decoder avc unsuported
    Define ro.charger.enable_suspend to true as charger no
    longer uses BOARD_CHARGER_ENABLE_SUSPEND.
    Bug: 153937817
    Signed-off-by: Redick Lin <redicklin@google.com>
    
    https://github.com/EdwinMoq/android_device_xiaomi_whyred/commit/8afafccb32a34d26d3f7daa3a0e310ed229c2f3e

--- system_prop.mk
+++ system_prop.mk
@@ -11,6 +11,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.voice.path.for.pcm.voip=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.charger.enable_suspend = true
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=7
 
